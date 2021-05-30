5b2aabb94eda2ac355b0152620b2ab96fe6e33b4
Bartłomiej Krakowski <bartlomiej@krakowski.dev>
2021-05-30 22:38:13 +0200

    feat: added arrays

--- p1/code.js
+++ p1/code.js
@@ -7,19 +7,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-function traverse(node) {
+const NAMES_WIDTH = [];
+const NAMES_HEIGHT = [];
+function traverse(node, name) {
     return __awaiter(this, void 0, void 0, function* () {
         if ("children" in node) {
             if (node.type !== "INSTANCE") {
                 for (const child of node.children) {
-                    traverse(child);
+                    traverse(child, name);
                 }
                 // TODO: Customizable node name
             }
-            else if (node.name === '_Specification') {
+            else if (node.name === name) {
                 const spaceComp = node.children.find(element => element.name === 'Line' || element.name === 'Space');
                 const textNode = node.children.find(element => element.type === "TEXT");
-                const textToDisplay = String(`${Math.max(spaceComp.width, spaceComp.height)}px`);
+                const valueToDisplay = NAMES_WIDTH.includes(name) ? spaceComp.width : NAMES_HEIGHT.includes(name) ? spaceComp.height : null;
+                const textToDisplay = String(`${valueToDisplay}px`);
                 let len = textNode.characters.length;
                 yield figma.loadFontAsync(textNode.getRangeFontName(0, 1));
                 textNode.deleteCharacters(0, len);
@@ -28,5 +31,7 @@ function traverse(node) {
         }
     });
 }
-traverse(figma.root);
+for (const selectedComp of figma.currentPage.selection) {
+    traverse(figma.root, selectedComp.name);
+}
 figma.closePlugin();
--- p1/code.ts
+++ p1/code.ts
@@ -1,14 +1,19 @@
-async function traverse(node) {
+const NAMES_WIDTH = []
+const NAMES_HEIGHT = []
+
+async function traverse(node, name) {
   if ("children" in node) {
     if (node.type !== "INSTANCE") {
       for (const child of node.children) {
-        traverse(child)
+        traverse(child, name)
       }
     // TODO: Customizable node name
-    } else if (node.name === '_Specification'){
+    } else if (node.name === name) {
       const spaceComp = node.children.find(element => element.name === 'Line' || element.name === 'Space')
       const textNode = node.children.find(element => element.type === "TEXT")
-      const textToDisplay = String(`${Math.max(spaceComp.width, spaceComp.height)}px`)
+      const valueToDisplay = NAMES_WIDTH.includes(name) ? spaceComp.width : NAMES_HEIGHT.includes(name) ? spaceComp.height : null
+
+      const textToDisplay = String(`${ valueToDisplay }px`)
 
       let len = textNode.characters.length
       await figma.loadFontAsync(textNode.getRangeFontName(0, 1))
@@ -20,5 +25,7 @@ async function traverse(node) {
   }
 }
 
-traverse(figma.root)
+for (const selectedComp of figma.currentPage.selection) {
+  traverse(figma.root, selectedComp.name)
+}
 figma.closePlugin()
